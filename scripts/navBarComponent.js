0d491f024e2bb5130519cc0cdba2ff8cbd0b9b9b
ilias jabri <68255254+iliass1234@users.noreply.github.com>
2022-03-16 13:31:50 +0100

    Create navBarComponent.js

---
+++ scripts/navBarComponent.js
@@ -0,0 +1,26 @@
+
+export class NavBar {
+    constructor(){
+        this.nav = document.createElement('nav');
+        this.childrenContainer = document.createElement('div')
+
+        this.nav.id = 'navBar';
+        this.nav.style = 'height: 50px; display: flex; justify-content: center; align-items: center; width: 100%; background-color: black;';
+        this.childrenContainer.style = 'display: flex; justify-content: center; align-items: center; height: 100%;width: 100%; max-width: 900px; background-color: red;'
+        this.nav.appendChild(this.childrenContainer)
+        
+    }
+
+    draw(whereToDraw){ 
+        whereToDraw.appendChild(this.nav);
+    }
+    
+    addBtn(innertext, theId){
+        this.navBtn = document.createElement('button');
+        this.navBtn.innerText = innertext;
+        this.navBtn.id = theId;
+        this.navBtn.style = ' border-radius: 5px; margin-left: 5px; padding: 8px; border: none; color: white; background-color: blue;';
+        this.childrenContainer.appendChild(this.navBtn);
+    }
+}
+
