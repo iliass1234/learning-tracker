92eed920086331dacbe6feaf2ef77d491aba68e5
ilias jabri <68255254+iliass1234@users.noreply.github.com>
2022-03-16 16:32:32 +0100

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,20 @@
-export class navBar {
-    constructor(){
-        this.nav = document.createElement('nav');
-        this.navBtn = document.createElement('button');
+import { NavBar } from "/scripts/navBarComponent.js";
+import { Canvas } from "/scripts/canvas.js";
 
-        this.navBtn.innerText = 'click here!';
-        this.navBtn.style = 'border: none; color: white; background-color: blue;';
-        this.nav.style = 'height: 50px; display: flex; justify-content: center; align-items: center; width: 100%; background-color: red;';
+let body = document.body;
+let navBar = new NavBar();
+let canvas = new Canvas();
 
-    }
+body.style.margin = '0';
+navBar.draw(body);
+canvas.draw(body);
 
-    draw(){
-        
-    }
-}
+let divition = document.createElement('div');
+divition.style = 'background-color: red; height: 40px; width: 50px;'
+canvas.addItem(divition)
+
+navBar.addBtn('hello', 'hello');
+navBar.addBtn('world', 'world');
+console.log('jjj')
+
 
