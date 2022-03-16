d3ac71bb2d23fc4430efd1876d2063f9748014fc
ilias jabri <68255254+iliass1234@users.noreply.github.com>
2022-03-16 13:32:17 +0100

    Create canvas.js

---
+++ scripts/canvas.js
@@ -0,0 +1,8 @@
+class Canvas {
+    constructor(){
+        this.canvas = document.createElement('main');
+        this.canvas.id = 'canvas';
+        this.canvas.style = 'height: 100%; width: 100%; background-color: green;';
+        
+    }
+}
